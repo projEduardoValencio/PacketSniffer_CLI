cc3d11ae755a0098b487d7733b199695c9d3aff0
Eduardo Valencio <eduardo.valencio.santos@gmail.com>
2025-10-20 18:45:00 -0300

    feat: Metrics implementation with Exceptions

---
+++ PacketSniffer/Shared/PacketException.cs
@@ -0,0 +1,19 @@
+﻿namespace PacketSniffer.Shared
+{
+    public class PacketException : Exception
+    {
+        public PacketException(string msg) : base(msg) { }
+    }
+
+    public class DeviceNotFoundException : PacketException {
+        public string DeviceName { get; set; }
+        public DeviceNotFoundException(string deviceName) : base($"Device {deviceName} not found")
+        {
+            DeviceName = deviceName;
+        }
+    }
+
+    public class DeviceNotOpened : PacketException {
+        public DeviceNotOpened() : base($"Device not opened") { }
+    }
+}
---
+++ PacketSniffer/Worker/Metrics.cs
@@ -0,0 +1,53 @@
+﻿using PacketSniffer.Interfaces;
+
+namespace PacketSniffer.Worker;
+
+public sealed class Metrics : IMetrics
+{
+    private long _packets;
+    private long _bytes;
+    private long _drops;
+    private long _lastPacket;
+    private long _lastBytes;
+    private DateTime _lastSnapshot = DateTime.UtcNow;
+    
+    // Interlocked atomic mult thread can access and modify
+    // Volatile get value without lock thread
+    public void IncPacket(int bytes)
+    {
+        Interlocked.Increment(ref _packets);
+        Interlocked.Add(ref _bytes, bytes);
+    }
+
+    public void IncDrop() => Interlocked.Increment(ref _drops);
+
+    public (long totalPkts, long totalBytes, double pps, double bps, long drops) Snapshot()
+    {
+        // Calc frame time
+        var now = DateTime.UtcNow;
+        var duration = (now - _lastSnapshot).TotalSeconds;
+        
+        // Short interval protection
+        if (duration <= 0.1) 
+        {
+            return (Volatile.Read(ref _packets), Volatile.Read(ref _bytes), 0, 0, Volatile.Read(ref _drops));
+        }
+
+        // Get AVG metrics
+        var packets = Volatile.Read(ref _packets);
+        var bytes = Volatile.Read(ref _bytes);
+
+        var deltaPacket = packets - _lastPacket;
+        var deltaBytes = bytes - _lastBytes;
+
+        var avgPacket = deltaPacket / duration;
+        var avgBytes = deltaBytes / duration;
+        
+        // Update properties
+        _lastSnapshot = now;
+        _lastPacket = packets;
+        _lastBytes = bytes;
+        
+        return (packets, bytes, avgPacket, avgBytes, Volatile.Read(ref _drops));
+    }
+}
